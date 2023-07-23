5ba71d875f3d088d659c6b1401ac50b4fb9964d1
Warren Smith <131816114+Wxrren@users.noreply.github.com>
2023-07-23 16:32:32 +0100

    Javascript file for a website homepage

---
+++ Creating a functional home page./functions.js
@@ -0,0 +1,4 @@
+//Remember Me function
+const rmCheck = document.getElementById("rememberME"),
+passInput = document.getElementById("password"), 
+emailInput = document.getElementById("email-address");
