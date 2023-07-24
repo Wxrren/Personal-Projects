aa3c9c39be4cc9e6c75a0a59ff284715ba73560a
Warren Smith <131816114+Wxrren@users.noreply.github.com>
2023-07-24 07:40:06 +0100

    Javascript file for a website homepage

--- Creating a functional home page./functions.js
+++ Creating a functional home page./functions.js
@@ -2,3 +2,30 @@
 const rmCheck = document.getElementById("rememberME"),
 passInput = document.getElementById("password"), 
 emailInput = document.getElementById("email-address");
+if(localStorage.checkbox && localStorage.checkbox !== "") {
+    rmCheck.setAttribute("checked", "checked");
+    passInput.value= localStorage.password;
+    emailInput.value= localStorage.email-address;
+} else {
+    rmCheck.removeAttribute("checked");
+    passInput.value = "";
+    emailInput.value = "";
+}
+
+function rememberME() {
+    if(rmCheck.checked && emailInput.value !=="" && passInput.value !=="") {
+        localStorage.email = emailInput.value;
+        localStorage.passInput = passInput.value;
+        localStorage.checkbox = rmCheck.value;
+    } else {
+        localStorage.email ="";
+        localStorage.passInput = "";
+        localStorage.checkbox = "";
+    }
+}
+
+
+
+
+
+  
